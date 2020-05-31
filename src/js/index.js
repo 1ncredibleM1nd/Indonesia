21aabe15b630d6a10a340fa489bbb185f7b1ceb8
Andrew <andierodin@gmail.com>
2020-05-31 15:41:54 +0300

    Add modal window, methods for opening and closing window by trigger, click and ESC

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
 $('.carousel').carousel()
+
+// Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', function() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        modal.style.display='block';
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+function closeModal() {
+
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    modal.style.display='none';
+    document.body.style.overflow = '';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
